22c1a9a33eeb5a191fda017055a9524b6b13d345
Pablo Bahamondes <62042364+pablosbw@users.noreply.github.com>
2025-11-25 23:02:19 -0300

    feat(users): Add creation endpoint for new users

--- racional-api/racional_api/urls.py
+++ racional-api/racional_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WithdrawCreateView, DepositCreateView, UserDetailView
+from .views import WithdrawCreateView, DepositCreateView, UserDetailView, UserListCreateView
 
 
 user_urls = [
+    path('users/', UserListCreateView.as_view(), name='user-list'),
     path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
 ]
 
--- racional-api/racional_api/views.py
+++ racional-api/racional_api/views.py
@@ -1,21 +1,37 @@
 from .models import Transaction, User
 from .serializers import DepositSerializer, UserSerializer
 from rest_framework import generics
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.response import Response
 from rest_framework import status
 
+@extend_schema_view(
+    get=extend_schema(
+        summary="Get all users",
+        description="Get all non deleted users",
+        responses=UserSerializer(many=True),
+    ),
+    post=extend_schema(
+        summary="Create a user",
+        description="Create a new user.",
+        request=UserSerializer,
+        responses={201: UserSerializer},
+    ),
+)
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.filter(is_deleted=False)
+    serializer_class = UserSerializer
 
 @extend_schema(
-    summary="Create, update or delete the user",
-    description="Create, update (partial allowed) or delete a user.",
+    summary="Retrieve, update or delete the user",
+    description="Retrieve, update (partial allowed) or delete a user.",
     request=UserSerializer,
     responses={200: UserSerializer, 204: None},
 )
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
     GET/PUT/PATCH/DELETE /api/users/<int:pk>/
-    Combined endpoint for Create, update and delete.
+    Combined endpoint for retrieve, update and delete.
     """
     queryset = User.objects.filter(is_deleted=False)
     serializer_class = UserSerializer
